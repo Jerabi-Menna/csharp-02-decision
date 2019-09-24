381d1ec0fb923907adad2cd0e68e4a0cd3b9d429
Jesse Rabidoux <jrabidoux1@nait.ca>
2019-09-24 11:41:45 -0600

    Chinese Zodiac Calculator

--- 30-2016F-IC-case-ChineseCalendar/ChineseCalendar.cs
+++ 30-2016F-IC-case-ChineseCalendar/ChineseCalendar.cs
@@ -10,6 +10,53 @@ namespace _30_2016F_IC_case_ChineseCalendar
     {
         static void Main(string[] args)
         {
+            Console.Write("What year were you born?: ");
+            int Year = int.Parse(Console.ReadLine());
+
+            int ChangeYear = (Year - 1900) % 12;
+
+            switch (ChangeYear)
+            {
+                case 0:
+                    Console.WriteLine("You are a sneaky rat");
+                    break;
+                case 1:
+                Console.WriteLine("You are a Hardy OOx");
+                    break;
+                case 2:
+                Console.WriteLine("You are a Majestic Tiger");
+                    break;
+                case 3:
+                Console.WriteLine("You are an agile rabbit");
+                    break;
+                case 4:
+                Console.WriteLine("You are a powerful dragon");
+                    break;
+                case 5:
+                Console.WriteLine("You are a slithering snake");
+                    break;
+                case 6:
+                Console.WriteLine("You are a rapid horse");
+                    break;
+                case 7:
+                Console.WriteLine("You are a leaping goat");
+                    break;
+                case 8:
+                Console.WriteLine("You are a swinging monkey");
+                    break;
+                case 9:
+                Console.WriteLine("You are a morning rooster");
+                    break;
+                case 10:
+                Console.WriteLine("You are a barking dog");
+                    break;
+                case 11:
+                Console.WriteLine("You are a Healthy Pig");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry?");
+                    break;
+            }
         }
     }
 }
